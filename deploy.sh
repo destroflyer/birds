d47ab166b6b8736a2c365f418315133a5b062ed8
carlkuesters <carl.kuesters@youengineering.com>
2020-09-05 01:58:44 +0200

    Finish deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Checkout
 git clone https://github.com/destroflyer/birds.git
 if [ -n "$1" ]; then
   git checkout $1
 fi
 
+# Build
 mvn clean install
+
+# Deploy
+rm -rf /var/www/destrostudios/apps/Birds/*
+mv assets /var/www/destrostudios/apps/Birds/
+mv target/birds-0.0.1.jar /var/www/destrostudios/apps/Birds/Birds.jar
+curl https://destrostudios.com:8080/apps/3/updateFiles
