56f4d22d3ec203963ad89d1d74fb2f66f0c1789b
carlkuesters <carl.kuesters@youengineering.com>
2020-09-05 02:51:16 +0200

    Cleanup deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+VERSION=$1
+CLIENT=$2
+
 # Checkout
 git clone https://github.com/destroflyer/birds.git
-if [ -n "$1" ]; then
-  git checkout $1
+if [ -n "$VERSION" ]; then
+  git checkout $VERSION
 fi
 
 # Build
 mvn clean install
 
 # Deploy
-rm -rf ${2}*
-mv assets ${2}
-mv target/birds-0.0.1.jar ${2}Birds.jar
+rm -rf ${CLIENT}*
+mv assets ${CLIENT}
+mv target/birds-0.0.1.jar ${CLIENT}Birds.jar
 curl https://destrostudios.com:8080/apps/3/updateFiles
